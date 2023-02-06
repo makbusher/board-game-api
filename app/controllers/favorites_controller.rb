4ee2e0030475e89ec4f390eda3707303c5262f21
Makayla <makaylabusher@gmail.com>
2023-02-06 13:59:11 -0600

    index displays current user favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,6 @@
 class FavoritesController < ApplicationController
   def index 
-    favorites = Favorite.all 
+    favorites = Favorite.where(user_id: current_user.id)
     render json: favorites.as_json
   end
   
