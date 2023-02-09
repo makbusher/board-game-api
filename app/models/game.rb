cd5e8ae9b17e0be033727408104f4932c603459e
Makayla <makaylabusher@gmail.com>
2023-02-09 11:54:42 -0600

    associations for reviews

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
   has_many :favorites
   has_many :users, through: :favorites
+  has_many :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :games, through: :favorites
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  has_many :reviews
 end
